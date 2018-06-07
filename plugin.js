838317af0a458c4313103aa739d8e51721e2aeed
Bob Gray <gray.bob98@gmail.com>
2018-06-06 22:44:35 -0500

    adding serialize function

--- plugin.js
+++ plugin.js
@@ -1,9 +1,17 @@
 "use strict";
 
-const querystring = require("querystring");
+const {URLSearchParams} = require("url"),
+	querystring = require("querystring");
 
 module.exports = {
 	contentType: "application/x-www-form-urlencoded",
+
+	serialize (request, response) {
+		var body = response.getBody(),
+			content = new URLSearchParams(body).toString();
+
+		request.proceed(content);
+	},
 
 	deserialize (request) {
 		return querystring.parse(request.getContent());
